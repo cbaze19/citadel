b28c70759b3e59aad9da90913c08392bc0856b95
Caleb Baze <cbaze19@gmail.com>
2015-10-13 21:32:42 -0500

    added about us paragraph, link to ctmrs, info gather

--- server.rb
+++ server.rb
@@ -8,20 +8,45 @@ get '/' do
 	erb :index
 end
 
-get '/list' do
-	erb :list
+get '/info' do
+	erb :info
+end
+
+post '/toctmrs' do
+	@name = params[:name]
+	@email = params[:email]
+	@phone = params[:phone]
+
+	subject = "Potential Customer - #{@name}"
+	body = "Name: #{@name}\nEmail: #{@email}\nPhone: #{@phone}"
+
+	email(subject, body)
+
+	redirect 'http://www.ctmrs.com/products.asp'
 end
 
 post '/contact' do
 	@name = params[:name]
 	@email = params[:email]
+	@phone = params[:phone]
 	@message = params[:message]
 	
+	subject = "Website Contact - #{@name}"
+	body = "#{@message}\n\nName: #{@name}\nEmail: #{@email}\nPhone: #{@phone}"
+
+	email(subject, body)
+	
+	flash[:confirmation] = "Thank you for your email.  We will get back to you as soon as possible!"
+
+	redirect '/'
+end
+
+def email(subject, body)
 	Pony.mail({
 		:to => 'contact@citadelmetalsupply.com',
 		:from => 'contact@citadelmetalsupply.com',
-		:subject => "Website Contact - #{@name}",
-		:body => "#{@message} \n\n #{@email}",
+		:subject => subject,
+		:body => body,
 		:via => :smtp,
 		:via_options => {
 			:address => 'smtp.gmail.com',
@@ -33,8 +58,4 @@ post '/contact' do
 			:domain => "localhost.localdomain"
 		}
 	})
-	
-	flash[:confirmation] = "Thank you for your email.  We will get back to you as soon as possible!"
-
-	redirect '/'
 end
