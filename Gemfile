831491223e2c5136ef7bc60b6cf6aa55305c30f5
Caleb Baze <cbaze19@gmail.com>
2022-07-18 21:26:10 -0500

    removed pony gem to see if issue

--- Gemfile
+++ Gemfile
@@ -1,5 +1,5 @@
 source 'https://rubygems.org'
 ruby '3.1.2'
 gem 'sinatra'
-gem 'pony'
+# gem 'pony'
 gem 'sinatra-flash'
--- server.rb
+++ server.rb
@@ -1,5 +1,5 @@
 require 'sinatra'
-require 'pony'
+# require 'pony'
 require 'sinatra/flash'
 
 enable :sessions
@@ -24,50 +24,50 @@ get '/contactUs' do
 	erb :contactUs
 end
 
-post '/toctmrs' do
-	@name = params[:name]
-	@email = params[:email]
-	@phone = params[:phone]
-
-	subject = "Potential Customer - #{@name}"
-	body = "Name: #{@name}\nEmail: #{@email}\nPhone: #{@phone}"
-
-	email(subject, body)
-
-	redirect 'http://www.ctmrs.com/products.asp'
-end
-
-post '/contact' do
-	@name = params[:name]
-	@email = params[:email]
-	@phone = params[:phone]
-	@message = params[:message]
-	
-	subject = "Website Contact - #{@name}"
-	body = "#{@message}\n\nName: #{@name}\nEmail: #{@email}\nPhone: #{@phone}"
-
-	email(subject, body)
-	
-	flash[:confirmation] = "Thank you for your email.  We will get back to you as soon as possible!"
-
-	redirect '/'
-end
-
-def email(subject, body)
-	Pony.mail({
-		:to => 'contact@citadelmetalsupply.com',
-		:from => 'contact@citadelmetalsupply.com',
-		:subject => subject,
-		:body => body,
-		:via => :smtp,
-		:via_options => {
-			:address => 'smtp.gmail.com',
-			:port => '587',
-			:enable_starttls_auto => true,
-			:user_name => 'contact@citadelmetalsupply.com',
-			:password => 'cms2015!!',
-			:authentication => :plain,
-			:domain => "localhost.localdomain"
-		}
-	})
-end
+# post '/toctmrs' do
+# 	@name = params[:name]
+# 	@email = params[:email]
+# 	@phone = params[:phone]
+#
+# 	subject = "Potential Customer - #{@name}"
+# 	body = "Name: #{@name}\nEmail: #{@email}\nPhone: #{@phone}"
+#
+# 	email(subject, body)
+#
+# 	redirect 'http://www.ctmrs.com/products.asp'
+# end
+#
+# post '/contact' do
+# 	@name = params[:name]
+# 	@email = params[:email]
+# 	@phone = params[:phone]
+# 	@message = params[:message]
+#
+# 	subject = "Website Contact - #{@name}"
+# 	body = "#{@message}\n\nName: #{@name}\nEmail: #{@email}\nPhone: #{@phone}"
+#
+# 	email(subject, body)
+#
+# 	flash[:confirmation] = "Thank you for your email.  We will get back to you as soon as possible!"
+#
+# 	redirect '/'
+# end
+#
+# def email(subject, body)
+# 	Pony.mail({
+# 		:to => 'contact@citadelmetalsupply.com',
+# 		:from => 'contact@citadelmetalsupply.com',
+# 		:subject => subject,
+# 		:body => body,
+# 		:via => :smtp,
+# 		:via_options => {
+# 			:address => 'smtp.gmail.com',
+# 			:port => '587',
+# 			:enable_starttls_auto => true,
+# 			:user_name => 'contact@citadelmetalsupply.com',
+# 			:password => 'cms2015!!',
+# 			:authentication => :plain,
+# 			:domain => "localhost.localdomain"
+# 		}
+# 	})
+# end
